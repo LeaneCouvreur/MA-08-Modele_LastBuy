ed6939aef73a66c9da60eb36197529e4b4006d8a
Gabriel Gloor <gabriel.gloor@cpnv.ch>
2022-01-29 16:15:53 +0100

    documentations

---
+++ Rendu/FileDatabase_LastDeal/privilege.sql
@@ -0,0 +1,12 @@
+Use LastDeal;
+SET IDENTITY_INSERT dbo.privileges ON;
+GO
+
+/*les données de cette table ont été automatiquement crées grâce au site: generatedata.com
+
+INSERT INTO [privileges] (id,name,score)
+VALUES
+  (1,'Platine,',647),
+  (2,'Or,',157),
+  (3,'Argent,',717),
+  (4,'Bronze,',792);
