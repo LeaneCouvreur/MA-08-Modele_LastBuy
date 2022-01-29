0fff92e2fd8e459c5064a3b6e56283bfe2b92dfd
Gabriel Gloor <gabriel.gloor@cpnv.ch>
2022-01-29 16:32:18 +0100

    contenus des tables

---
+++ Rendu/SelectDatabase_LastDeal.sql
@@ -0,0 +1,38 @@
+/*
+Gabriel Gloor
+29.01.22
+Projet-MA-08
+*/
+
+/*dans ce fichier vous trouverez les commandes nécessaires
+  pour afficher le contenus de chacune de nos tables.*/
+
+/*Pour la table customers*/
+SELECT * FROM customers;
+
+/*Pour la table: privileges*/
+SELECT * FROM privileges;
+
+/*Pour la table: aministrators*/
+SELECT * FROM administrators;
+
+/*Pour la table: whishlists*/
+SELECT * FROM whishlists;
+
+/*Pour la table: termsOfPayement*/
+SELECT * FROM termsOfPayement;
+
+/*Pour la table: orderStatus*/
+SELECT * FROM orderStatus;
+
+/*Pour la table: articles*/
+SELECT * FROM articles;
+
+/*Pour la table: orders*/
+SELECT * FROM orders;
+
+/*Pour la table: whishlists_has_articles*/
+SELECT * FROM whishlists_has_articles;
+
+/*Pour la table: orders_has_articles*/
+SELECT * FROM orders_has_articles;
